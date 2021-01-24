73ecd6d8329c6d26f9c69f7c52344077fca95ef7
Daniel Gadd <danielgadd@outlook.com>
2021-01-24 12:08:09 +0000

    debugging slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,9 +69,10 @@ export default function Home({ allPostsData }) {
             <div className = {styles.wall}></div>
           </div>
           <div className = {styles.right}>
-            <Slider />
+          <Slider />
           </div>
         </section>
+        <Slider />
       </Layout>
     </motion.div>
   );
@@ -85,54 +86,3 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   };
 };
-
-{/* <motion.div
-initial = {{ opacity: 0 }}
-animate = {{ opacity: 1 }}
-exit = {{ opacity: 0 }}>
-<Layout home>
-  <Head>
-    <title>{siteTitle}</title>
-  </Head>
-  <section className = {styles.contain}>
-    <div className = {styles.earth}>
-      <div className = {styles.wrapper}>
-        <h1 style = {{margin:"0px 0px 40px 0px"}}>The Earth Foundation</h1>
-        <h2>Inspire. Educate. Mentor. Empower.</h2>
-        <div className = {styles.summary}>
-          <p style = {{margin:"40px 0px 40px 0px"}}>The Earth Foundation aims to distribute $1 million annually to students, researchers​, and youngentrepreneurs​ with innovative ideas to tackle environmental challenges. </p>
-          <p>By encouraging andsupporting these projects, The Earth Foundation strives to foster a self-perpetuating ecosystemthat accelerates positive change​ towards ​environmental sustainability.</p>
-        </div>
-      </div>
-    </div>
-  </section>
-  {content.home.map(item => (
-    <Board
-      image = {item.image}
-      index = {item.index}
-      label = {item.label}
-      summary = {item.summary}
-      background = {item.background}
-      key = {item.index} />
-  ))}
-  <section className = {styles.latest}>
-    <div className = {styles.control}>
-      <div><FontAwesomeIcon icon = {faArrowLeft} /></div>
-      <div><FontAwesomeIcon icon = {faArrowRight} /></div>
-      <div>
-        <span>Latest News</span>
-      </div>
-      <div>
-        <span>Read All News</span>
-        <div><FontAwesomeIcon icon = {faArrowRight} /></div>
-      </div>
-    </div>
-    <div className = {styles.docker}>
-      <div className = {styles.wall}></div>
-      <div className = {styles.car}>
-        <Slider />
-      </div>
-    </div>
-  </section>
-</Layout>
-</motion.div> */}
