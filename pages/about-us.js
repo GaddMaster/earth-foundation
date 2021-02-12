8208fc34c1e30fa2aebbdcec33e0b4f1df9d0473
Wladimir <wladimir.code@gmail.com>
2021-02-12 16:05:28 -0300

    chore: New branch about-us

--- pages/about-us.js
+++ pages/about-us.js
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const AboutUs = ({ isVisible }) => {
   console.log(isVisible);
-  return <h1>About Us</h1>;
+  return <h1>About Us.</h1>;
 };
 
 export default AboutUs;
