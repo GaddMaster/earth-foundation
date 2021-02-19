6700c022399c7af4bd43e1fe9aa7c92391d2d568
Wladimir <wladimir.code@gmail.com>
2021-02-19 14:40:20 -0300

    feat: AboutUs page, css adjustments

--- pages/about-us.js
+++ pages/about-us.js
@@ -7,10 +7,10 @@ import Layout from "../components/Layout";
 import Switcher from "../components/Switcher";
 import BlueSwitcher from "../components/BlueSwitcher";
 import Section from "../components/Section";
-import ContentSection from "../components/ContentSection";
-import Subscribe from "../components/Subscribe";
 
 import foundation from "../assets/foundation";
+
+import styles from "../styles/about.module.scss";
 
 class AboutUs extends PureComponent {
 
@@ -18,42 +18,54 @@ class AboutUs extends PureComponent {
 
     return (
       <motion.div
-        initial = {{ opacity: 0 }}
-        animate = {{ opacity: 1 }}
-        exit = {{ opacity: 0 }}>
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}>
 
         <Head>
           <title>The Earth Foundation | About Us | Landing</title>
         </Head>
 
-        <Layout title = "The Earth Foundation Background">
+        <Layout title="The Earth Foundation Background">
 
           <Section
-            cover = "/images/jeremy-bishop-dR_q93lfaTw-unsplash.jpg"
-            title = "The Earth Foundation Background"
-            paragraphs = {[
+            cover="/images/jeremy-bishop-dR_q93lfaTw-unsplash.jpg"
+            title="The Earth Foundation Background"
+            paragraphs={[
               "The passion and concern that young people have about environmental sustainability became very evident during the series of global school strikes which spread across the world in 2019, inspired by the actions of environmental activist Greta Thunberg."
             ]} />
 
-          <ContentSection
-            image = "/images/about_inspire_generation.png"
-            paragraphs = {[
-              "The Earth Foundation will take this enthused youth and​ inspire, educate, mentor, and empower it to effect real change and hopefully, in turn, embolden the following generation."
-            ]} />
+
+          <section className={styles.section}>
+            <div className={styles.cover} >
+              <div className={styles.wrapper}>
+                <div className={styles.summary}>
+                  <p>
+                    The Earth Foundation will take this enthused youth and​ inspire, 
+                    educate, mentor, and empower it to effect real change and hopefully, 
+                    in turn, embolden the following generation.
+                  </p>
+                </div>
+                <div className={styles.image}>
+                  <img src="/images/about_inspire_generation.png" />
+                </div>
+              </div>
+            </div>
+          </section>
 
 
-          <BlueSwitcher 
-            title = "The Earth Foundation Board" 
-            titleColor = "#16172C" 
-            data = {foundation.board} />
+          <BlueSwitcher
+            title="The Earth Foundation Board"
+            titleColor="#16172C"
+            data={foundation.board} />
 
-          <Switcher 
-            title = "The Earth Foundation Team" 
-            titleColor = "#CFAA7A"
-            data = {foundation.team} />
+          <Switcher
+            title="The Earth Foundation Team"
+            titleColor="#CFAA7A"
+            data={foundation.team} />
 
         </Layout>
-        
+
       </motion.div>
     )
   }
