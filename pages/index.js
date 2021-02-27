e4549b4203d02200639cb7131e92054edb03056a
Daniel Gadd <danielgadd@outlook.com>
2021-02-27 17:53:05 +0000

    moo tracker

--- pages/index.js
+++ pages/index.js
@@ -130,6 +130,10 @@ class Home extends PureComponent {
                 <Layout home>
                     <Head>
                         <title>The Earth Foundation | Home | Landing</title>
+                        <script>
+                            !function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
+                            mootrack('init', '8b288103b7524c08bdc330d41a7b0d9f');
+                        </script>
                     </Head>
                     <h1>The Earth Foundation</h1>
                     <h2>Coming Soon</h2>
