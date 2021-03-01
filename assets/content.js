a2f7dfc8317c7fcba14b0f8be8b3b2da802c3765
Daniel Gadd <danielgadd@outlook.com>
2021-03-01 15:46:57 +0000

    home page styling

--- assets/content.js
+++ assets/content.js
@@ -13,28 +13,56 @@ const home = {
         {
             image : "/images/annie-spratt-Tno1Zd3T6yY-unsplash.webp",
             label: "The Earth Prize",
-            summary: "A $200,000 global youth environmental sustainability competition for studentsworldwide between the ages of 13 and 19 years.",
+            summary: "A $200,000 global youth environmental sustainability competition for students worldwide between the ages of 13 and 19 years",
+            links: [
+                {
+                    label: "Find out about The Earth Prize Mentors",
+                    link: "/#"
+                },
+                {
+                    label: "Find out about The Earth Prize Mentors",
+                    link: "/#"
+                }
+            ],
             path: "/",
             background: "#16172C"
         },
         {
             image : "/images/geran-de-klerk-qzgN45hseN0-unsplash.webp",
             label: "The Earth Foundation Awards",
-            summary: "$300,000 in research grants ​and scholarships to university studentsand researchers​.",
+            summary: "$300,000 in research grants and scholarships for university students and researchers​",
+            links: [
+                {
+                    label: "Read more about the initiative",
+                    link: "/#"
+                }
+            ],
             path: "/",
             background: "#074D3F"
         },
         {
             image : "/images/magnezis-magnestic-TW62wXQ6Omc-unsplash.webp",
-            label: "The Earth Foundation Incubator Initiative",
-            summary: "The Foundation will distribute $500,000 in seed-fundingto accelerate the launch of revolutionary products, organizations, and enterprises worldwide.",
+            label: "The Earth Foundation Incubator",
+            summary: "The Foundation will distribute $500,000 in seed-funding to accelerate the launch of revolutionary products, organizations and enterprises worldwide",
+            links: [
+                {
+                    label: "Read more about the initiative",
+                    link: "/#"
+                }
+            ],
             path: "/",
             background: "#1A3324"
         },
         {
             image : "/images/pam-ivey-c0Y30cWbyEc-unsplash.webp",
             label: "The Earth Foundation Alumni Association",
-            summary: "A mentoring and networking platform that connects ourparticipants, partners, and ambassadors​.",
+            summary: "A networking platform that connects our participants, mentors, partners, and ambassadors",
+            links: [
+                {
+                    label: "Read more about the initiative",
+                    link: "/#"
+                }
+            ],
             path: "/",
             background: "#02304F"
         }
