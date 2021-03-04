34ce9edf2a9542162161b73765e2a4b3165f2b24
rajeevrajchal <rajeevrajchal12@gmail.com>
2021-03-04 18:02:21 +0545

    page created.

---
+++ pages/thank-you.jsx
@@ -0,0 +1,14 @@
+import Layout from "components/Layout";
+
+const ThankYou = () => {
+    return (
+        <Layout
+            title = "Welcome to our community"
+            header
+        >
+
+        </Layout>
+    );
+};
+
+export default ThankYou;
