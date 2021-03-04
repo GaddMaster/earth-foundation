dbf8582f5550469ec54432ca08e865f57eaeb562
Daniel Gadd <danielgadd@outlook.com>
2021-03-04 11:40:18 +0000

    minor fix

--- pages/apply.jsx
+++ pages/apply.jsx
@@ -58,14 +58,129 @@ const MentorApplication = () => {
             footer>
 
             <div className = {styles.container}>
+                <div className = {styles.sub}>
+                    <span>become a mentor to the next generation</span>
+                </div>
+                <div className = {styles.title}>
+                    <span>the earth prize program mentorship application form</span>
+                </div>
+                <div className = {styles.divider}></div>
 
-            {/* My Header COmponent */}
+                <div className = {`${styles.box} ${styles.wrap}`}>
+                    <div className = {styles.label}>
+                        <span>First Name</span>
+                    </div>
+                    <input
+                        value = {first}
+                        placeholder = "Your first name here"
+                        onChange = {e => onFirst(e.target.value)} />
+                </div>
 
-            {/* My Circles component */}
+                <div className = {`${styles.box} ${styles.wrap}`}>
+                    <div className = {styles.label}>
+                        <span>Last Name</span>
+                    </div>
+                    <input
+                        value = {last}
+                        placeholder = "Your last name here"
+                        onChange = {e => onLast(e.target.value)} />
+                </div>
 
-            {/* Your rows which float left and right */}
+                <div className = {`${styles.box} ${styles.wrap}`}>
+                    <div className = {styles.label}>
+                        <span>Date of Birth</span>
+                    </div>
+                    <input
+                        value = {dob}
+                        placeholder = "Select your birth date"
+                        onChange = {e => onDOB(e.target.value)} />
+                </div>
 
-            {/* My circle text component */}
+                <div className = {`${styles.box} ${styles.wrap}`}>
+                    <div className = {styles.label}>
+                        <span>Email Address</span>
+                    </div>
+                    <input
+                        value = {email}
+                        placeholder = "Your email here"
+                        onChange = {e => onEmail(e.target.value)} />
+                </div>
+
+                <div className = {styles.box}>
+                    <div className = {styles.label}>
+                        <span>Academic Institution</span>
+                    </div>
+                    <input
+                        value = {institution}
+                        placeholder = "Academic Institution"
+                        onChange = {e => onInstitution(e.target.value)} />
+                </div>
+
+                <div className = {`${styles.box} ${styles.wrap}`}>
+                    <div className = {styles.label}>
+                        <span>Title of degree you are pursuing</span>
+                    </div>
+                    <input
+                        value = {degree}
+                        placeholder = "Degree title here"
+                        onChange = {e => onDegree(e.target.value)} />
+                </div>
+
+                <div className = {`${styles.box} ${styles.wrap}`}>
+                    <div className = {styles.label}>
+                        <span>Expected graduation date</span>
+                    </div>
+                    <input
+                        value = {expect}
+                        placeholder = "Choose date"
+                        onChange = {e => onExpect(e.target.value)} />
+                </div>
+
+                <div className = {styles.box}>
+                    <div className = {styles.label} style = {{marginBottom:40}}>
+                        <span>Which environmental topics are you most knowledgeable about?</span>
+                    </div>
+                    <div className = {styles.topics}>
+                        {topics.map(topic => (
+                            <ButtonBase
+                                className = {`${styles.button} ${styles[Boolean(selected.find(t => t === topic))]}`}
+                                onClick = {onTopic.bind(this, topic)}
+                                key = {topic.replace(/ /g, '-')}>
+                                <span>{topic}</span>
+                            </ButtonBase>
+                        ))}
+                    </div>
+                </div>
+
+                <div className = {styles.box} style = {{marginTop:-40}}>
+                    <textarea
+                        value = {other}
+                        placeholder = "Other..."
+                        onChange = {e => onOther(e.target.value)} />
+                </div>
+
+                <div className = {styles.box}>
+                    <div className = {styles.label}>
+                        <span>Please tell us why you want to become part of The Earth Prize Mentorship Program</span>
+                    </div>
+                    <textarea
+                        value = {why}
+                        placeholder = "..."
+                        onChange = {e => onWhy(e.target.value)} />
+                    <span>250 - 500 words</span>
+                </div>
+
+                <ButtonBase
+                    className = {styles.submit}
+                    onClick = {onSubmit}>
+                        <span>Submit Application</span>
+                </ButtonBase>
+
+                {success && 
+                    <div className = {styles.success}>
+                        <span>{success}</span>
+                    </div> 
+                }
             
             </div>
 
