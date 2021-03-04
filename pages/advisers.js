a65b34683c18064f597124413f7752ca121b41cf
princess may bulaong <bulaongprincessmay@gmail.com>
2021-03-04 15:20:50 +0800

    edits

--- pages/advisers.js
+++ pages/advisers.js
@@ -1,8 +1,8 @@
 
 import Head from "next/head";
 import Layout from "../components/Layout";
+import TitleDesc from "components/TitleDesc";
 
-import { motion } from "framer-motion";
 import styles from "../styles/advisers.module.scss";
 import GridItem from '../components/Card/GridItem'
 import Subscribe from "../components/Subscribe";
@@ -12,37 +12,31 @@ import CardAdvisers from "../components/CardAdvisers";
 
 const Advisers = props => {
     return (
-        <motion.div
-            initial = {{ opacity: 0 }}
-            animate = {{ opacity: 1 }}
-            exit = {{ opacity: 0 }}>
+       
+            
             <Layout title = "The Earth Foundation Advisers"
+                    background = "whitesmoke"
                     header
                     footer>
-                <div className = {styles.container}>
-                    <div className = {styles.top}>
-                        <div className = {styles.header}>
-                            <div className = {styles.label}>
-                                <h1>The Earth Foundation Advisors</h1>
-                            </div>
-                            <div className = {styles.line}></div>
-                        </div>
-                        <div className = {styles.summary}>
-                            <span>The Earth Foundation's mission is to inspire, educate, mentor, and empower youth to accelerate positive change towards environmental sustainability. In achieving this mission, The Earth Foundation has surrounded itself with renowned experts i the field of environmental sustainability, to help guide its strategic development.</span>
-                        </div>
-                        <div>
-                      </div>
-                   </div>
+                    <TitleDesc
+                    title = "The Earth Foundation Advisors"
+                    desc = "The Earth Foundation's mission is to inspire, educate, mentor, and empower youth to accelerate positive change towards environmental sustainability. In achieving this mission, The Earth Foundation has surrounded itself with renowned experts i the field of environmental sustainability, to help guide its strategic development."
+                    color = {{
+                        title: "#17152B",
+                        desc: "#17152B"
+                    }} />
+            
+                
               <div className = {styles.cards}>
                 <GridItem  >
                     <CardAdvisers />
                     </GridItem>
              </div>           
-             </div>   
+            
                    
             <Subscribe />
             </Layout>
-        </motion.div>
+      
     );
 };
 
